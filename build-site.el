29ffd8135e7f2813d87a9570b9a50d879caa5067
Ben Apacible <ben@apacible.net>
2024-05-14 17:02:31 +1000

    Added build-site.el

---
+++ build-site.el
@@ -0,0 +1,10 @@
+(setq org-publish-project-alist
+      (list
+       (list "my-org-site"
+             :recursive t
+             :base-directory "./content"
+             :publishing-directory "./public"
+             :publishing-function 'org-html-publish-to-html)))
+
+;; Generate the site output
+(org-publish-all t)
